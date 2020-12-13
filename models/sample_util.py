de138e43b1f7499d18570c224d68aed0b9a6480e
SuperJohnZhang <yzhangch@connect.ust.hk>
2020-12-14 02:51:30 +0800

    fix bugs

--- models/sample_util.py
+++ models/sample_util.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 
+epsilon = 1e-8
 
 # issues to be finished
 # 1. here we assume 1 order is generated each time now
@@ -94,70 +95,100 @@ def generatePosMap(mask, params):
 # ------------------------ map related finished -----------------------------------
 # ------------------------ reward related start -----------------------------------
 # this function returns the reward and state in the next step based on
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
 def oneStepMove(pre_pos_map, people_action):
     h, w = pre_pos_map.shape
-    pre_pos_ind = np.array(np.where(pre_pos_map==1))
+    pre_pos_ind = np.array(np.where(pre_pos_map==1)).reshape([2])
     new_pos_ind = pre_pos_ind
-    if people_action.data[0, 0] == 1: # move up
-        new_pos_ind[1] = np.max(new_pos_ind[0]-1, 0)
-    elif people_action.data[0, 1] == 1: # move down
-        new_pos_ind[0] = np.min(new_pos_ind[0]+1, h)
-    elif people_action.data[0, 2] == 1: # move left
-        new_pos_ind[1] = np.max(new_pos_ind[1]-1, 0)
-    elif people_action.data[0, 3] == 1: # move right
-        new_pos_ind[0] = np.min(new_pos_ind[1]+1, w)
+    # cast people action to prob 
+    people_action = softmax(people_action.numpy().reshape([4,1]))
+    prob = np.random.uniform(0,1)
+
+    if prob <= people_action[0]: # move up
+        new_pos_ind[0] = np.max([new_pos_ind[0]-1, 0])
+    elif prob <= np.sum(people_action[0:2]): # move down
+        new_pos_ind[0] = np.min([new_pos_ind[0]+1, h-1])
+    elif prob <= np.sum(people_action[0:3]): # move left
+        new_pos_ind[1] = np.max([new_pos_ind[1]-1, 0])
+    else: # move right
+        new_pos_ind[1] = np.min([new_pos_ind[1]+1, w-1])
     new_pos_map = np.zeros_like(pre_pos_map)
-    new_pos_map[new_pos_ind] = 1
+    new_pos_map[tuple(new_pos_ind)] = 1
     return new_pos_map, new_pos_ind
 
 def getNextStateReward(last_state, pickup_controls, people_actions, params):
     h, w = params['h'], params['w']
     # we generate the initial state
     # the channel order of states are
-    # map
-    # pickup map
-    # money map
-    # iternate: position of deliever, pickup position of deliever, money position of deliever
-    last_state = last_state.data
+    # map 0
+    # pickup map 1
+    # money map 2
+    # iternate: position of deliever, pickup position of deliever, money position of deliever 3*(i+1)
+    last_state = last_state.numpy()
     states = np.zeros((1, 3+3*params["num_delivers"], h, w), np.float32)
     states[0, 0] = last_state[0, 0]     # map doesn't change
     states[0, 1] = generatePickupMap(sample_params).reshape([h, w]) # generate new pickup
     states[0, 2] = generateMoneyMap(sample_params).reshape([h, w]) # generate new money
     rewards = 0
 
+    # we need to determine pickup controls
+    control = params["num_delivers"]
+    pickup_controls = softmax(pickup_controls.numpy().reshape([params["num_delivers"]+1, 1]))
+    prob = np.random.uniform(0, 1)
+    if prob<pickup_controls[0]:
+        control = 0
+    for i in range(len(pickup_controls)):
+        if prob < np.sum(pickup_controls[:params["num_delivers"]-i+1]):
+            continue
+        else:
+            control = params["num_delivers"]-i+1
+            break
+    # print("the control is")
+    # print(control)
+    # print(prob)
+    # print(pickup_controls)
+
     for i in range(params["num_delivers"]):
         # update pos map
-        new_pos_map, new_pos_ind = oneStepMove(states[0, 3*(i+1)], people_actions[i])
+        new_pos_map, new_pos_ind = oneStepMove(last_state[0, 3*(i+1)], people_actions[i])
+        # print(new_pos_ind)
         states[0, 3*(i+1)] = new_pos_map
 
         # update pick up m
         pre_pickup_m = last_state[0, 1+3*(i+1)]
-        if pickup_controls.data[0, i]==1:
+        if control == i:
             last_pickup_m = last_state[0, 1]
             pre_pickup_m = pre_pickup_m + last_pickup_m
             pre_pickup_m[new_pos_ind] = 0
             states[0, 1+3*(i+1)] = pre_pickup_m
-        if pre_pickup_m[new_pos_ind] != 0:
-            rewards = rewards + pre_pickup_m[new_pos_ind]
-            pre_pickup_m[new_pos_ind] = 0
+        if pre_pickup_m[tuple(new_pos_ind)] != 0:
+            rewards = rewards + pre_pickup_m[tuple(new_pos_ind)]
+            # print(pre_pickup_m[tuple(new_pos_ind)])
+            pre_pickup_m[tuple(new_pos_ind)] = 0
         states[0, 1+3*(i+1)] = pre_pickup_m
 
         # update money m
         pre_money_m = last_state[0, 2+3*(i+1)]
-        if pickup_controls.data[0, i]==1:
+        if control == i:
             pre_money_m = last_state[0, 2+3*(i+1)]
             last_money_m = last_state[0, 2]
             pre_money_m = pre_money_m + last_money_m
-        if pre_money_m[new_pos_ind] != 0:
-            rewards = rewards + pre_money_m[new_pos_ind]
-            pre_money_m[new_pos_ind] = 0
+        if pre_money_m[tuple(new_pos_ind)] != 0:
+            rewards = rewards + pre_money_m[tuple(new_pos_ind)]
+            # print(pre_money_m[tuple(new_pos_ind)])
+            pre_money_m[tuple(new_pos_ind)] = 0
         states[0, 2+3*(i+1)] = pre_money_m
         
         # calculate rewards
         rewards = rewards + states[0, 0, new_pos_ind[0], new_pos_ind[1]]
+        # print(states[0, 0, new_pos_ind[0], new_pos_ind[1]])
 
     # when the order is reject
-    if pickup_controls[params["num_delivers"]] == 1:
+    if control == params["num_delivers"]:
         rewards = rewards + params["reject_cost"]
     return states, rewards.reshape([1, 1])
 
@@ -197,15 +228,19 @@ def SampleEpisode(model, params=sample_params, duration=250):
     # all_pickup_controls = []
     # all_people_actions = []
 
-
     for i in range(duration):
         # convert states from np into tensor
         last_state = torch.from_numpy(all_states[-1])
         last_state = last_state.cuda()
         pickup_controls, people_actions = model.actor(last_state)
+        # pickup_controls = np.random.randn(1, 4)
+        # pickup_controls = torch.from_numpy(pickup_controls)
+        # people_actions = np.random.randn(4, 1, 4)
+        # people_actions = torch.from_numpy(people_actions)
 
         # get the rewards and next state in np structure
         states, rewards = getNextStateReward(last_state, pickup_controls, people_actions, params)
+        print('cut:{reward}'.format(reward=rewards[0]))
         all_states.append(states)
         all_rewards.append(rewards)
         # all_pickup_controls.append(pickup_controls.numpy())
@@ -219,5 +254,6 @@ def SampleEpisode(model, params=sample_params, duration=250):
     # eposide['pickup_controls'] = torch.from_numpy(np.concatenate(all_pickup_controls, axis=0))
     # eposide['people_actions'] = torch.from_numpy(np.concatenate(all_people_actions, axis=0))
     # eposide['donws'] = torch.from_numpy(np.concatenate(all_dones, axis=0))
-    
     return eposide
+
+# SampleEpisode(0)
